fe74d3bd1b4d01f195e9e7c71d822177678abf5e
imanishbarnwal <manishbarnwal725@gmail.com>
2021-01-13 09:57:50 +0530

    Updated comments

--- sort-list.py
+++ sort-list.py
@@ -1,6 +1,6 @@
 numbers = [1, 3, 4, 2, 8, 9, 7, 6] 
 
 # Sorting list of Integers in ascending 
-numbers.sort() 
+numbers.sort()  # For strings just place strings instead of numbers
 
 print(numbers) 
